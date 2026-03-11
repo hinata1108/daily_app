6df05fdace692b82366d4c46ac7a36d35a94dc45
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-11 15:40:19 +0900

    FormのUi完成

---
+++ src/components/Daily_Form/Choice_Colors/ChoiceColor.tsx
@@ -0,0 +1,21 @@
+import {colors } from '../../../constant/colors'
+import './ChoiceColor.css'
+
+type Props = {
+    selectColor: string;
+    setSelectColor: (color: string) => void;
+}
+
+export const ChoiceColor = ({ selectColor, setSelectColor }: Props) => {
+
+    return (
+        <div className="ChoiceColor">
+            {colors.map((color)=> (
+                <button key={color} 
+                className={`color ${selectColor === color ? "selected" : ""} ${selectColor && selectColor !== color ? "notSelected" : ""}`}
+                onClick={()=> setSelectColor(color)}
+                style={{ backgroundColor: color }}/>
+            ))}
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Header } from './components/Header.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
+    <App />
   </StrictMode>,
 )
