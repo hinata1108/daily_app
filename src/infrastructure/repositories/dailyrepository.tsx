71e2bd704429cce20dcb8e3a415b09f7891aa781
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-20 14:56:14 +0900

    dailyrepojitory

---
+++ src/infrastructure/repositories/dailyrepository.tsx
@@ -0,0 +1,21 @@
+import {supabase} from '../infrastructure/supabaseClient';
+
+// 日記の取得
+export const getDailies = async () => {
+    const {data, error} = await supabase
+        .from('dailies').select("*").order("created_at",{ascending: false})
+    if(error) {
+        throw error
+    }
+    return data
+}
+
+// 日記の作成
+export const createDaily = async (title:string,color:string,content:string) => {
+    const {data, error} = await supabase
+        .from('dailies').insert({title,color,content})
+    if(error) {
+        throw error
+    }
+    return data
+}
