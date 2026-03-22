5e9ff3ef6abb8aae4bbc75d085d8f480351ee227
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-22 17:26:09 +0900

    fix：日本語の画像も送れるようにした

--- src/infrastructure/repositories/imageRepository.tsx
+++ src/infrastructure/repositories/imageRepository.tsx
@@ -1,7 +1,8 @@
 import {supabase} from "../supabaseClient";
 
 export const uploadImage = async (file:File) => {
-    const filename=`diary/${Date.now()}_${file.name}`;
+    const ext= file.name.split('.').pop();
+    const filename = `daily/${Date.now()}.${ext}`;
     // 画像アップロード
     const {error}= await supabase.storage.from("images").upload(filename, file);
     if(error){
