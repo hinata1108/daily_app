dfd31ff63e08f7c98eb534b2e150d6bfe9cdc332
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-22 12:25:29 +0900

    imageRepository

---
+++ src/infrastructure/repositories/imageRepository.tsx
@@ -0,0 +1,13 @@
+import {supabase} from "../supabaseClient";
+
+export const uploadImage = async (file:File) => {
+    const filename=`diary/${Date.now()}_${file.name}`;
+    const {error}= await supabase.storage.from("images").upload(filename, file);
+    if(error){
+        throw error;
+    }
+
+    const {data}= await supabase.storage.from("images").getPublicUrl(filename);
+
+    return data.publicUrl;
+}
