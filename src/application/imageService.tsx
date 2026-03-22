7837d4bb5b0c3e32cf6bf765f9493e2fca78863f
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-22 12:29:05 +0900

    imageServiceけした

--- src/application/imageService.tsx
+++
@@ -1,7 +0,0 @@
-import { uploadImage } from "../infrastructure/repositories/imageRepository";
-
-// 画像のアップロード
-export const uploadImageFile = async (file: File) => {
-    const imageURL = await uploadImage(file);
-    return imageURL;
-}
