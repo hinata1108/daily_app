aef5420b9fcd5eea3c3f5ed4ae32ae49cac8446f
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-23 00:01:27 +0900

    css完成

--- src/presentation/components/footer/footer.tsx
+++ src/presentation/components/footer/footer.tsx
@@ -4,15 +4,15 @@ export const Footer = () => {
     return (
     <footer className="footer">
         <div className="footer-content">
-            <Link to="/home" className="footer-link">
-            <img src="home.svg" alt="Home" className="footer-icon" />
+            <Link to="/home" className="link">
+            <img src="home.svg" alt="Home" className="icon" />
             
             </Link>
-            <Link to="/write" className="footer-link">
-            <img src="write.svg" alt="Write" className="footer-icon" />
+            <Link to="/write" className="link">
+            <img src="write.svg" alt="Write" className="bigIcon" />
             </Link>
-            <Link to="/calendar" className="footer-link">
-            <img src="calendar.svg" alt="Calendar" className="footer-icon" />
+            <Link to="/calendar" className="link">
+            <img src="calendar.svg" alt="Calendar" className="icon" />
             </Link>
         </div>
     </footer>
