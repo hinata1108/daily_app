002b8f05f31e1a7df5b87c8b062cd824a1e947c3
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-22 16:03:50 +0900

    dailyrepositoryにfileたした

--- src/infrastructure/repositories/imageRepository.tsx
+++ src/infrastructure/repositories/imageRepository.tsx
@@ -2,6 +2,7 @@ import {supabase} from "../supabaseClient";
 
 export const uploadImage = async (file:File) => {
     const filename=`diary/${Date.now()}_${file.name}`;
+    // 画像アップロード
     const {error}= await supabase.storage.from("images").upload(filename, file);
     if(error){
         throw error;
